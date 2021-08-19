0824663e2bb77183c06999f93bed04a9a29dd1c3
AllanKirui <nallawilljr@gmail.com>
2021-08-19 14:48:15 +0300

    Add functionality to menu toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,42 @@
+// A. Burger functionality
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+const body = document.querySelector("body");
+
+console.log(menu);
+console.log(menu);
+console.log(menu.children[0]);
+console.log(menu.children[0].children[0]);
+burger.addEventListener("click", () => {
+   burger.classList.toggle("active");
+   if (burger.classList.contains("active")) {
+      body.classList.toggle("no-scroll");
+      menu.style.visibility = "visible";
+      menu.children[0].style.transform = "scale(1)";
+      menu.children[0].style.transitionDuration = "0.75s";
+      // menu.children[0].setAttribute("transform", "scale(1)");
+      menu.children[0].children[0].style.opacity = "1";
+      menu.children[0].children[0].style.transition = "opacity 0.3s ease";
+   } else {
+      body.classList.remove("no-scroll");
+      burger.classList.remove("active");
+      menu.style.visibility = "hidden";
+      menu.children[0].style.transform = "scale(0)";
+      menu.children[0].style.transitionDuration = "0.75s";
+      // menu.children[0].setAttribute("transform", "scale(1)");
+      menu.children[0].children[0].style.opacity = "0";
+      menu.children[0].children[0].style.transition = "opacity 0.3s ease";
+   }
+   // burger.classList.toggle("active");
+   // menu.style.visibility = "visible";
+   // menu.children[0].style.transform = "scale(1)";
+   // menu.children[0].style.transitionDuration = "0.75s";
+   // // menu.children[0].setAttribute("transform", "scale(1)");
+   // menu.children[0].children[0].style.opacity = "1";
+   // menu.children[0].children[0].style.transition = "opacity 0.3s ease";
+});
+
+// B. Clock functionality
 // Use the setInterval function to call the specified function
 // every second
 setInterval(setClock, 1000);
@@ -6,9 +45,9 @@ setInterval(setClock, 1000);
 const secondHand = document.querySelector("[data-second-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const hourHand = document.querySelector("[data-hour-hand]");
-const day_el = document.querySelector(".day");
-const time_el = document.querySelector(".time");
-const date_el = document.querySelector(".date");
+const dayEl = document.querySelector(".day");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
 
 // Create lists to hold the days in a week and months in a year
 const days = [
@@ -57,9 +96,12 @@ function setClock() {
    setRotation(hourHand, hoursRatio);
 
    // Set the time for the digital clock
-   day_el.innerHTML = `${days[day]}`;
-   time_el.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
-   date_el.innerHTML = `${months[month]} <span class="circle-bg">${
+   dayEl.innerHTML = `${days[day]}`;
+   // timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+   timeEl.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
+      minutes < 10 ? `0${minutes}` : minutes
+   }`;
+   dateEl.innerHTML = `${months[month]} <span class="circle-bg">${
       date < 10 ? `0${date}` : date
    }</span> ${year}`;
 }
